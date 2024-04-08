3b0de106a6077ec480d74e1ad46f15ddc61a078e
XiaoStack <820399825@qq.com>
2024-04-08 23:58:11 +0800

    [add] 新增 html 比对展示

--- src/main/java/com/example/springgraycomment/controller/TestController.java
+++ src/main/java/com/example/springgraycomment/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.example.springgraycomment.controller;
 
+import com.example.springgraycomment.common.GrayConstant;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +21,7 @@ public class TestController {
      * getUser
      */
     @GetMapping(value = "getUser")
-    public String getUser() {
-        System.out.println("comment-getUser");
-        return "comment-getUser";
+    public String getUser(@RequestHeader(required = false) String grayTag) {
+        return GrayConstant.GRAY_VALUE.equals(grayTag) ? "来自灰度服务的评论" : "来自正常服务的评论";
     }
 }
