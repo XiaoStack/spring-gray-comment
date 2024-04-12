29a6ddb95fb2d37dcf13c58149049681ee5a1de5
XiaoStack <820399825@qq.com>
2024-04-12 11:55:54 +0800

    [update] 代码优化

--- src/main/java/com/example/springgraycomment/controller/TestController.java
+++ src/main/java/com/example/springgraycomment/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.example.springgraycomment.controller;
 
 import com.example.springgraycomment.common.GrayConstant;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +19,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/test")
 public class TestController {
     /**
+     * 当前环境
+     */
+    @Value("${spring.cloud.nacos.discovery.metadata.grayTag:false}")
+    private String grayTagConfig;
+
+    /**
      * getComment
      */
     @GetMapping("/getComment")
     public String getComment(@RequestHeader(required = false) String grayTag) {
-        return GrayConstant.GRAY_VALUE.equals(grayTag) ? "来自灰度服务的评论" : "来自正常服务的评论";
+        System.out.println("header grayTag:" + grayTag);
+        boolean isGray = GrayConstant.GRAY_VALUE.equals(grayTagConfig);
+        String serviceName = isGray ? "comment-gray" : "comment-normal";
+        System.out.println("当前环境:" + serviceName);
+        return "加载自服务 " + serviceName + " 的评论：" + (isGray ? "灰度灰度" : "正常正常");
     }
 }
